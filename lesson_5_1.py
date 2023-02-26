b7cde61346e5b8c15a8114d63afa46fd84163b83
Pavel Apanas <apanaspavel@gmail.com>
2023-02-26 20:27:57 +0300

    Homework

---
+++ lesson_5_1.py
@@ -0,0 +1,23 @@
+def decimal_to_binary(decimal):
+    binary = ''
+    if decimal == 0:
+        return '0'
+    while decimal > 0:
+        binary = str(decimal % 2) + binary
+        decimal //= 2
+    return binary
+
+
+def binary_to_decimal(binary):
+    decimal = 0
+    for i in range(len(binary)):
+        decimal += int(binary[len(binary)-1-i]) * 2**i
+    return decimal
+
+
+def converter(decimal):
+    binary = decimal_to_binary(decimal)
+    decimal_from_binary = binary_to_decimal(binary)
+    return f'{decimal} в двоичной системе: {binary}, {binary} в десятичной системе: {decimal_from_binary}'
+
+print(converter(1200))  # 10 в двоичной системе: 1010, 1010 в десятичной системе: 10
